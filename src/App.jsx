720e5d69a5f60ec61beb165ac256378a26568474
dahlia <dahlia@dahlias-MacBook-Pro.local>
2026-09-08 11:40:14 -0600

    added username to profile card.

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,29 @@ function App() {
 
   return (
     <div className="app">
+
+      {/* profile card */}
       <div className="flex h-30 w-90 bg-indigo-400 rounded-2xl mx-auto my-40">
-        <div className="w-27 flex flex-col justify-center items-center">
+
+        {/* avatar section */}
+        <div className="flex flex-col w-27 pt-2 justify-center items-center">
           <img
             className='w-20 aspect-square rounded-full object-cover '
             src={heidiProf.profilePic}
             alt='profile picture' />
-          <span className='text-white-200 text-sm '>Username</span>
+
+          {/* username */}
+          <div className='pt-1'>
+            <span className='inline-block w-25 truncate text-sm font-medium '>{heidiProf.userName}</span>
+          </div>
+
         </div>
+
+        {/* Name and profession */}
         <div className="bg-blue-400 w-63">b</div>
+
       </div>
+
     </div>
   )
 }
