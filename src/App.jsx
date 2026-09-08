951fc07d8bb1cc219cc4b18fdb6a9365f23a11e9
dahlia <dahlia@dahlias-MacBook-Pro.local>
2026-09-08 11:10:43 -0600

    format

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@ import felix from './assets/felix.png'
 
 function App() {
 
-   const felixProf = {
-              firstName:'Felix',
-              lastName:'Turbowitz',
-              userName:'@dontshakemysalt',
-              profession:'Exotic Arms Dealer',
-              profilePic:felix,
-          }
-      const heidiProf = {
-              firstName:'Heidi',
-              lastName:'Kraemer',
-              userName:'@bungaloShampew8)', 
-              profession:'Wild Hampster Tamer',
-              profilePic:heidi,
-          }
+  const felixProf = {
+    firstName: 'Felix',
+    lastName: 'Turbowitz',
+    userName: '@dontshakemysalt',
+    profession: 'Exotic Arms Dealer',
+    profilePic: felix,
+  }
+  const heidiProf = {
+    firstName: 'Heidi',
+    lastName: 'Kraemer',
+    userName: '@bungaloShampew8)',
+    profession: 'Wild Hampster Tamer',
+    profilePic: heidi,
+  }
 
   return (
     <div className="app">
       <div className="flex h-30 w-90 bg-indigo-400 rounded-2xl mx-auto my-40">
         <div className="w-27 flex flex-col justify-center items-center">
-          <img 
-          className='w-20 aspect-square rounded-full object-cover '
-          src={heidiProf.profilePic}
-          alt='profile picture'/>
+          <img
+            className='w-20 aspect-square rounded-full object-cover '
+            src={heidiProf.profilePic}
+            alt='profile picture' />
           <span className='text-white-200 text-sm '>Username</span>
         </div>
         <div className="bg-blue-400 w-63">b</div>
